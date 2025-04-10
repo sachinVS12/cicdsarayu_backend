77ba03c06fda037b08130240eb672154dd0e90a4
Sachin <vs1sachi12@gmail.com>
2025-04-10 16:58:45 +0530

    change new

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors({
+    origin : "*",
+    methods : ["GET","POST","PUT","PATCH","DELETE"]
+}))
+
+app.get("/",(req,res)=>{
+    res.status(200).json({success:true,message:"Hello World 13"})
+})
+
+app.listen(4000,()=>{
+    console.log("Listening on port 4000");
+})
