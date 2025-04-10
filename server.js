d836ccc2cf53e2ec5b5ccee323afa4a081b2679d
Sachin <vs1sachi12@gmail.com>
2025-04-10 17:16:46 +0530

    change numbe1r

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors({
 }))
 
 app.get("/",(req,res)=>{
-    res.status(200).json({success:true,message:"Hello World 14"})
+    res.status(200).json({success:true,message:"Hello World 14000"})
 })
 
 app.listen(4000,()=>{
